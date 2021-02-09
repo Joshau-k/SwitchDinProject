1da7ec68fd70f689f3d6c5714e6689cb493b147a
Josh <Josh@C44>
2021-02-09 23:42:34 +1100

    Refactor of html and javascript

--- WebSubscriber/MQQTSubscriber.js
+++ WebSubscriber/MQQTSubscriber.js
@@ -1,35 +1,12 @@
-
 const broker = 'mqtt.eclipseprojects.io';
+let client = new Paho.MQTT.Client(broker, 80, "webSubscriber");
 
-// Create a client instance
-client = new Paho.MQTT.Client(broker, 80, "webSubscriber");
-// client = new Paho.MQTT.Client(location.hostname, Number(location.port),  broker);
-
-// set callback handlers
-// client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
-
-// connect the client
-client.connect({onSuccess:onConnect});
-// client.connect({onSuccess:onConnect});
-
-
-
-// called when the client connects
 function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
   console.log("onConnect");
   client.subscribe("Number23847923");
 }
 
-// called when the client loses its connection
-// function onConnectionLost(responseObject) {
-//   if (responseObject.errorCode !== 0) {
-//     console.log("onConnectionLost:"+responseObject.errorMessage);
-//   }
-// }
-
-// called when a message arrives
-// function onMessageArrived(message) {
-//   console.log("onMessageArrived:"+message.payloadString);
-// }
+function mqqtConnect() {
+  client.onMessageArrived = onMessageArrived;
+  client.connect({onSuccess:onConnect});
+}
